260c71f18f75a0242eb01e5490adaff3afa18bf8
Pandelis Zembashis <p@ndel.is>
2024-01-27 14:12:13 +0000

    gravity!

--- Assets/Gravity.cs
+++ Assets/Gravity.cs
@@ -1,44 +1,20 @@
 using UnityEngine;
-using Unity.Physics;
-
-public class Example : MonoBehaviour
-{
-    Rigidbody m_Rigidbody;
-    public float m_Thrust = 20f;
-
-    void Start()
-    {
-        //Fetch the Rigidbody from the GameObject with this script attached
-        m_Rigidbody = GetComponent<Rigidbody>();
-    }
-
-    void FixedUpdate()
-    {
-        if (Input.GetButton("Jump"))
-        {
-            //Apply a force to this Rigidbody in direction of this GameObjects up axis
-            m_Rigidbody.AddForce(transform.up * m_Thrust);
-        }
-    }
-}
 
 public class Gravity : MonoBehaviour
 {
-    Rigidbody m_Rigidbody;
-    public float m_Thrust = 20f;
+
 
     void Start()
     {
-        //Fetch the Rigidbody from the GameObject with this script attached
-        m_Rigidbody = GetComponent<Rigidbody>();
+
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        if (Input.GetButton("Jump"))
-        {
-            //Apply a force to this Rigidbody in direction of this GameObjects up axis
-            m_Rigidbody.AddForce(transform.up * m_Thrust);
+        if (Input.GetKey("space")) {
+
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0);
+        
         }
     }
 }
