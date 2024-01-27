e91390f32292dc0603cea8f8888be74f90575dc8
Pandelis Zembashis <p@ndel.is>
2024-01-27 14:51:28 +0000

    fix

--- Assets/Gravity.cs
+++ Assets/Gravity.cs
@@ -2,19 +2,31 @@ using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
+    Rigidbody2D m_Rigidbody;
+    public float m_Thrust = 4;
 
 
     void Start()
     {
-
-    }
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+}
 
     void Update()
     {
         if (Input.GetKey("space")) {
 
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0);
+            this.m_Rigidbody.AddForce(transform.up * this.m_Thrust);
         
+        }
+
+        if(Input.GetKey("right"))
+        {
+            this.m_Rigidbody.AddForce(transform.right * this.m_Thrust);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            this.m_Rigidbody.AddForce(-transform.right * this.m_Thrust);
         }
     }
 }
